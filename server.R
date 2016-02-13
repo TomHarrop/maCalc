76d259fdc4b310b84aeadab659bed50e460bee35
Tom Harrop <twharrop@gmail.com>
2016-02-13 20:27:16 +0100

    Added more sliders

--- server.R
+++ server.R
@@ -2,93 +2,101 @@ library(shiny)
 library(ggplot2)
 library(gridExtra)
 
+# calculate radians from degrees
+rad <- function(degrees) {
+  degrees * pi / 180
+}
+
+# mechanical advantage function
+CalculateMechanicalAdvantage <- function(
+  L, YO, PA, PO, DO, delta, W) {
+  # Computes the mechanical advantage
+  #
+  # Args:
+  #   PA = cantilever arm length in mm
+  #   DO = vertical distance from canti studs to centre of rim in mm
+  #   YO = yoke height (measured from canti studs) in mm
+  #   PO = fork width at canti studs in mm
+  #   delta = angle between canti arm and brake pad attachment arm
+  #   L = length of brake pad from brake pad attachment arm to rim in mm
+  #       (15 ~ 20 mm)
+  #   W = rim width in mm
+  # Returns:
+  #   Mechanical advantage (float)
+  
+  # calculate alpha, i.e. the canti angle (degrees from vertical. e.g. a
+  # canti arm parallel with the fork has alpha == 0)
+  alpha <- (180 * atan(DO / ((0.5 * PO) - (0.5 * W) - L)) / pi) +
+    delta - 90
+  # calculate the MA
+  numerator <- YO - (cos(rad(alpha)) * PA)
+  denominator <- (sin(rad(alpha)) * PA ) + (0.5 * PO)
+  gamma <- atan(numerator / denominator) * 180 / pi
+  beta <- gamma + 90 - alpha
+  term.1 <- sin(rad(gamma)) ^ -1
+  term.2 <- sin(rad(beta))
+  term.3 <- PA / DO
+  term.1 * term.2 * term.3
+}
+
+OptimiseMechanicalAdvantage <- function(
+  vars, target.ma, PA = PA, PO = PO, DO = DO, delta = delta, W = W) {
+  # Wrapper to CalculateMechanicalAdvantage() for optim() call
+  # Args:
+  #   vars = vector of opt.YO (YO) and opt.L (L)
+  #   target.ma = desired mechanical advantage
+  #   PA = cantilever arm length in mm
+  #   DO = vertical distance from canti studs to centre of rim in mm
+  #   PO = fork width at canti studs in mm
+  #   delta = angle between canti arm and brake pad attachment arm
+  #   W = rim width in mm
+  # Returns:
+  #   Difference between target.ma and calculated ma
+  
+  opt.YO <- vars["YO"]
+  opt.L <- vars["L"]
+  
+  ma <- CalculateMechanicalAdvantage(
+    L = opt.L, YO = opt.YO, PA = PA, PO = PO, DO = DO, delta = delta,
+    W = W)
+  
+  abs(target.ma - ma)
+}
+
 # input$target.ma: desired mechanical advantage
 
 shinyServer(
   function(input, output) {
     output$ma.plot <- renderPlot({
-      # default values for now
+
+
       # brown bomba
-      YO <- 115
-      PO <- 80
-      DO <- 30
-      W <- 30
+      # YO <- 115
+      YO <- input$yo
+      # PO <- 80
+      PO <- input$po
+      DO <- input$do
+      #DO <- 30
+      W <- input$w
+      # W <- 30
+
       # BR-CX70
-      PA <- 65 ## measured
+      PA <- input$pa
+      #PA <- 65 ## measured
       delta <- 20.5 ## measured
-
-      # calculate radians from degrees
-      rad <- function(degrees) {
-        degrees * pi / 180
-      }
-
-      # mechanical advantage function
-      CalculateMechanicalAdvantage <- function(
-        L, YO, PA, PO, DO, delta, W) {
-        # Computes the mechanical advantage
-        #
-        # Args:
-        #   PA = cantilever arm length in mm
-        #   DO = vertical distance from canti studs to centre of rim in mm
-        #   YO = yoke height (measured from canti studs) in mm
-        #   PO = fork width at canti studs in mm
-        #   delta = angle between canti arm and brake pad attachment arm
-        #   L = length of brake pad from brake pad attachment arm to rim in mm
-        #       (15 ~ 20 mm)
-        #   W = rim width in mm
-        # Returns:
-        #   Mechanical advantage (float)
-
-        # calculate alpha, i.e. the canti angle (degrees from vertical. e.g. a
-        # canti arm parallel with the fork has alpha == 0)
-        alpha <- (180 * atan(DO / ((0.5 * PO) - (0.5 * W) - L)) / pi) +
-          delta - 90
-        # calculate the MA
-        numerator <- YO - (cos(rad(alpha)) * PA)
-        denominator <- (sin(rad(alpha)) * PA ) + (0.5 * PO)
-        gamma <- atan(numerator / denominator) * 180 / pi
-        beta <- gamma + 90 - alpha
-        term.1 <- sin(rad(gamma)) ^ -1
-        term.2 <- sin(rad(beta))
-        term.3 <- PA / DO
-        term.1 * term.2 * term.3
-      }
-
-      OptimiseMechanicalAdvantage <- function(
-        vars, target.ma, PA = PA, PO = PO, DO = DO, delta = delta, W = W) {
-        # Wrapper to CalculateMechanicalAdvantage() for optim() call
-        # Args:
-        #   vars = vector of opt.YO (YO) and opt.L (L)
-        #   target.ma = desired mechanical advantage
-        #   PA = cantilever arm length in mm
-        #   DO = vertical distance from canti studs to centre of rim in mm
-        #   PO = fork width at canti studs in mm
-        #   delta = angle between canti arm and brake pad attachment arm
-        #   W = rim width in mm
-        # Returns:
-        #   Difference between target.ma and calculated ma
-
-        opt.YO <- vars["YO"]
-        opt.L <- vars["L"]
-
-        ma <- CalculateMechanicalAdvantage(
-          L = opt.L, YO = opt.YO, PA = PA, PO = PO, DO = DO, delta = delta,
-          W = W)
-
-        abs(target.ma - ma)
-      }
-
+      delta <- input$delta
+      
       # variables to optimise: length of brake pad from brake pad attachment arm
       # to rim and yoke height
-      vars <- c(L = 15, YO = 115)
+      vars <- c(L = 15, YO = YO)
 
       # run the optimisation
       optimised.ma <- optim(par = vars,
                             fn = OptimiseMechanicalAdvantage,
                             PA = PA, PO = PO, DO = DO, delta = delta, W = W,
                             target.ma = input$target.ma,
-                            lower = c(15, vars["YO"]),
-                            upper = c(25, vars["YO"] + 50), method = "L-BFGS-B")
+                            lower = c(25, vars["YO"]),
+                            upper = c(33, vars["YO"] + 50), method = "L-BFGS-B")
 
       ma.final <- input$target.ma - optimised.ma$value
       L.final <- optimised.ma$par["L"]
--- ui.R
+++ ui.R
@@ -2,7 +2,7 @@ library(shiny)
 
 fluidPage(
   titlePanel("Calculate mechanical advantage", windowTitle = "MA Calculator"),
-
+  
   sidebarLayout(
     # inputs go in the sidebar in sliders
     sidebarPanel(
@@ -11,11 +11,44 @@ fluidPage(
                   min = 1,
                   max = 3,
                   value = 2.5,
-                  round = 1,
-                  step = 0.1)
-    ),
+                  step = 0.1),
+      sliderInput("yo",
+                  "Minimum yoke height",
+                  min = 50,
+                  max = 300,
+                  value = 150,
+                  step = 1),
+      sliderInput("po",
+                  "Fork width at canti studs",
+                  min = 10,
+                  max = 150,
+                  value = 80,
+                  step = 1),
+      sliderInput("do",
+                  "Vertical distance from canti studs to centre of rim",
+                  min = 20,
+                  max = 40,
+                  value = 30,
+                  step = 1),
+      sliderInput("w",
+                  "Rim width",
+                  min = 15,
+                  max = 50,
+                  value = 30,
+                  step = 0.1),
+      sliderInput("pa",
+                  "Cantilever arm length",
+                  min = 50,
+                  max = 100,
+                  value = 65,
+                  step = 1),
+      sliderInput("delta",
+                  "Angle between canti arm and brake pad attachment arm",
+                  min = 0,
+                  max = 90,
+                  value = 20.5,
+                  step = 0.1)),
 
-    # plot goes in the main panel
     mainPanel(
       plotOutput("ma.plot")
     )
